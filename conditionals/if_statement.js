34e8e31e49d105298c8664bf7ca2774e55822baf
dev_josh <Alanajoshua535@gmail.com>
2022-12-11 15:31:27 +0100

    conditional statements

---
+++ conditionals/if_statement.js
@@ -0,0 +1,11 @@
+
+let rand = Math.random();
+if(rand < 0.5){
+    console.log("your number is less than 0.5")
+    console.log(rand);
+}
+
+if (rand >= 0.5) {
+    console.log("your number is greater than 0.5")
+    console.log(rand);
+}
