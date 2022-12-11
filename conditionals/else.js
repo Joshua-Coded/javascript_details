7e5866eceeaf4674d350a1c74dcf2048cc2a8496
dev_josh <Alanajoshua535@gmail.com>
2022-12-11 16:10:41 +0100

    else  statements

---
+++ conditionals/else.js
@@ -0,0 +1,14 @@
+ const dayOfWeek = prompt("enter a day of the week")
+
+ if(dayOfWeek === "Monday") {
+     console.log("i hate mondays")
+ } 
+ else if(dayOfWeek === "Saturday") {
+     console.log("i love saturdays")
+ } 
+ else if(dayOfWeek === "Friday") {
+    console.log("i love Fridays, they are decent after work.")
+ } 
+else {
+    console.log("meh not a great day")
+}
