23da3fd95359e1c4eed969ac10bba8a8e46eed54
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-29 01:45:01 +0200

    functions in js

--- functions/script.js
+++ functions/script.js
@@ -1,19 +1,19 @@
 // function template
 
-function singSong() {
-    console.log('done');
-    console.log('done');
-    console.log('done');
-    console.log('done');
-}
+// function singSong() {
+//     console.log('done');
+//     console.log('done');
+//     console.log('done');
+//     console.log('done');
+// }
 
 // function with arguments parameters
 
-function greet(person) {
-    console.log(`Firstname is: ${person}`)
-}
+// function greet(person) {
+//     console.log(`Firstname is: ${person}`)
+// }
 
-assignment
+// assignment
 /*
 Define a function called rant which accepts a string argument called message. 
  The function should print out an uppercased version of message 3 times 
@@ -21,10 +21,26 @@ Define a function called rant which accepts a string argument called message.
 For example, rant("I hate beets") should print
 */
 
-function rant(message) {
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-}
-rant("i hate beets");
+// function rant(message) {
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+// }
+// rant("i hate beets");
 
+
+// functino for creating simple interest
+
+
+// function simpleInterest(p, r, t)
+
+
+function simpleInterest(p, r, t) {
+    let si = (p * r * t) / 100;
+    return si;
+};
+
+// execution
+
+console.log(simpleInterest(1000, 5, 2));
+console.log(simpleInterest(1000, 6.7, 3));
