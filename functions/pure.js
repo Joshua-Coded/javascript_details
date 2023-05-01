6ca4d67889b861f0926744d1d7813fa22dab0c0f
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-01 22:27:06 +0200

    pure function

--- functions/pure.js
+++ functions/pure.js
@@ -1,10 +1,27 @@
 // pure functions
 
-function isValidLogin(username, password){
-    if(username == "joshua" && password == 12345)
-    return true;
-    else
-    return false;
+// function isValidLogin(username, password){
+//     if(username == "joshua" && password == 12345)
+//     return true;
+//     else
+//     return false;
+// }
+
+// console.log(isValidLogin("joshua", 12345));
+
+// callback functions
+
+function callBack1(name, age){
+    console.log(`Name is ${name}, age is ${age}`)
 }
 
-console.log(isValidLogin("joshua", 12345));
+function callBack2(name) {
+    console.log(`Name is ${name}`)
+}
+
+function doWork(name, age) {
+    name = "Doc" + name;
+    age++;
+    callBack2(name)
+}
+doWork("Joshua", 21);
