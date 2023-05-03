03ec43fc34d41c81417a76461c85565e35a20706
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-03 15:08:47 +0200

    obj

---
+++ object_oriented/accesing_property.js
@@ -0,0 +1,8 @@
+// accessing properties of the object
+const customers = {
+    customerName: "Scott",
+    email: "scott@gmail.com"
+};
+
+// console.log(customers.customerName, customers.email);
+console.log(customers["customerName"], customers["email"]);
