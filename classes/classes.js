d7a38e297901148dea9ef9f4e6019ac143aad5a9
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-12 12:19:41 +0200

    second object

--- classes/classes.js
+++ classes/classes.js
@@ -21,7 +21,12 @@ let employee1 = new Employee();
 employee1.empId = 123;
 employee1.empName = "Smith";
 employee1.salary = 1000;
-console.log(employee1);
-console.log(employee1.getEmployeeName());
 employee1.getSalary(2000);
 console.log(employee1);
+
+let employee2 = new Employee(); // second object for our classes
+employee2.empName = "John";
+employee2.salary = 7000;
+employee2.empId = 232;
+employee2.getSalary(4000);
+console.log(employee2);
