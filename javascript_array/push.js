7a33ae3e0def134132eb6c3b094607e914b5037f
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-16 08:30:53 +0200

    pop

---
+++ javascript_array/push.js
@@ -0,0 +1,8 @@
+// The push method allows you to add element at the end of array
+// [].push(value);
+
+const prices = [65,45,78,34,56,32,56];
+prices.push(897)
+console.log(prices);
+prices.pop();
+console.log(prices);
