d68b5a8ce8c64869ccd0526578a5e151ffb9a1e8
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-16 08:40:43 +0200

    pop

--- javascript_array/push.js
+++ javascript_array/push.js
@@ -6,3 +6,18 @@ prices.push(897)
 console.log(prices);
 prices.pop();
 console.log(prices);
+
+// array of objects
+const products = [
+    {productName: "TV", price: 643},
+    {productName: "TV STAND", price: 50},
+    {productName: "Battery", price: 43},
+    {productName: "Remote", price: 64},
+    {productName: "LCD", price: 103}
+];
+
+console.log("Before push", products);
+products.push({productName: "CABLE", price: 100})
+console.log("After push", products);
+console.log(products.pop())
+console.log("After pop", products);
