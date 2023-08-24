62dad68d773cf7fcdf4bbd9f576ec23f26918798
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-25 01:50:13 +0200

    tv maze saerch app

---
+++ ajax/API/tv_search_app/index.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('click', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = {params: {q: searchTerm}};
+   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    for ( let result of shows ) {
+        if (result.show.image){
+            const img = document.createElement('IMG');
+    img.src = result.show.image.medium;
+    document.body.append(img);
+        }
+        
+    }
+}
