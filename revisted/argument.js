e13ee038b85ab90cc5fa45516aff10f5590b817e
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-26 23:53:20 +0200

    using the argument keyword ☑️

--- revisted/argument.js
+++ revisted/argument.js
@@ -1,12 +1,15 @@
 // always use the arguments keyword when you are not sure of the input from users.
 
 function getCardBill(){
+    let sum = 0;
     for(let i = 0; i < arguments.length; i++){
-        console.log(arguments[i]);
+        // console.log(arguments[i]);
+        sum += arguments[i];
     }
+    return sum;
     // console.log(arguments);
 }
 
-getCardBill(12,43);
-getCardBill(1,2,3,4,5,6);
-getCardBill(43,65,78,98,86);
+console.log(getCardBill(12,43));
+console.log(getCardBill(1,2,3,4,5,6));
+console.log(getCardBill(43,65,78,98,86));
