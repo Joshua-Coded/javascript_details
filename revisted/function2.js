af6d9a35a337da78b1dc1845f355a2f120324e5f
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-26 23:35:12 +0200

    calling another function ☑️

--- revisted/function2.js
+++ revisted/function2.js
@@ -1,9 +1,19 @@
-function sayName(){
-    console.log("my name is joshua alana, the best software engineer");
-    tellAge();
+// function sayName(){
+//     console.log("my name is joshua alana, the best software engineer");
+//     tellAge();
+// }
+
+// function tellAge(){
+//     console.log("I am : " + 10);
+// }
+// sayName();
+
+function sqaure(n){
+    return n * n;
 }
 
-function tellAge(){
-    console.log("I am : " + 10);
+function cube(n){
+    return sqaure(n) * n;
 }
-sayName();
+console.log(sqaure(3));
+console.log(cube(4));
